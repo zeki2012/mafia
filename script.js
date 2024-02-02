41df3cbf2a8442a6d806b24b14132f41208f5948
= <=>
2024-02-02 21:27:12 +0400

    loobbb

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let ad = prompt('Adinizi qeyd edin')
-function zeki() {
-    let kart = Math.floor(Math.random() * 3); 
-    let result;
+function guessNumber() {
+    let userInput = document.getElementById("guess").value;
+    let randomNumber= Math.floor(Math.random() * 3);
+    let player = '';
 
-    switch (kart) {
-        case 1:
-            result = `${ad} Komisarsiniz. Vezifeniz mafyanı tapmaq öldürmək`;
-            break;
-        case 2:
-            result = `${ad} Mafyasiniz. Vezifəniz insanları öldürmək`;
-            break;
-        default:
-            result = `${ad} Vetendassiniz.Vezifeniz mafyanı tapmaq səs vermək`;
-            break;
+    if (randomNumber == 0) {
+        player = 'mafia';
+    } else if (randomNumber == 1) {
+        player = 'komisar';
+    } else {
+        player = 'vetendas';
     }
 
-    document.getElementById("mafya").innerHTML = result;
+    if (userInput == player) {
+        document.getElementById('netice').innerHTML = `Təbriklər!! Siz ${player}-i tapdiniz`
+
+    } else {
+        document.getElementById('netice').innerHTML = `Cox teessuf siz ${player}-i oldurdunuz`
+    }  
+
 }
